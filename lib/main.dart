ea1ec2262af3a0e170d80f58ee9c76611b7c61fd
ahmedtaju <ahmmedahmd02@gmail.com>
2024-12-29 15:16:39 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MyApp(),
       ),
     );
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
